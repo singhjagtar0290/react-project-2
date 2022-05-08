9338b784505bc4e01ac686abf8b5c41d16e92b56
jagtar <singhjagtar0290@gmail.com>
2022-05-08 16:54:56 +0530

    Calling dynaimic classes

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import Card from "../UI/Card";
+import classes from './AddUser.module.css'
 const AddUser = (props) => {
 
     const addUserHandler = (event) => {
@@ -7,6 +8,7 @@ const AddUser = (props) => {
     }
 
     return(
+        <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
             <input id="username" type="text" />
@@ -14,6 +16,7 @@ const AddUser = (props) => {
             <input id="username" type="number" />
             <button type="submit">Add User</button>
         </form>
+        </Card>
     )
 }
 
